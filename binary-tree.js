8d93d10d3885d47dc4541cda4710ac7a32bed17b
Lada <mladda@mail.ru>
2016-01-27 19:38:50 +0200

    added constructor, insert and contains methods

--- binary-tree.js
+++ binary-tree.js
@@ -2,15 +2,56 @@
 
 class BinaryTree {
 	constructor() {
-
+		 this.root = null;
 	}
 
 	insert(data) {
 
+		if (this.root == null){
+			this.root = new Node();
+			this.root.data = data;
+		} else {
+			var current = this.root;
+
+			while (true){
+				var newNode = new Node();
+				newNode.data = data;
+				var parent = current;
+
+				if (data < current.data){
+					current = current.left;
+
+					if (current == null){
+						parent.left = newNode;
+						return;
+					}
+				} else {
+					current = current.right;
+
+					if (current == null){
+						parent.right = newNode;
+						return;
+					}
+				}
+			}
+		}
+
 	}
 
 	contains(data) {
-
+		var current = this.root;
+		while (current.data != data){
+			if (data < current.data){
+				current = current.left;
+			}
+			else {
+				current = current.right;
+			}
+			if (current == null){
+				return false;
+			}
+		}
+		return true;
 	}
 
 	remove(data) {
