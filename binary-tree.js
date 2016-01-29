ec3bf43857ec78b318118ede996a6a3d896674ca
Lada <mladda@mail.ru>
2016-01-29 15:07:21 +0200

    added isEmpty and remove methods

--- binary-tree.js
+++ binary-tree.js
@@ -7,7 +7,7 @@ class BinaryTree {
 
 	insert(data) {
 
-		if (this.root == null){
+		if (this.root === null){
 			this.root = new Node();
 			this.root.data = data;
 		} else {
@@ -54,15 +54,116 @@ class BinaryTree {
 		return true;
 	}
 
+
 	remove(data) {
+		function getSuccessor(nodeForDel){
+			var successorParent = nodeForDel;
+			var successor = nodeForDel;
+			var current = nodeForDel.right;
+			while (current != null){
+				successorParent = successor;
+				successor = current;
+				current = current.left;
+			}
+			if (successor != nodeForDel.right){
+				successorParent.left = successor.right;
+				successor.right = nodeForDel.right;
+			}
+			return successor;
+		}
+		var parent = this.root;
+		var current = this.root;
+		var toLeft = true;
+
+		while(current.data != data){
+			parent = current;
+
+			if (data < current.data){
+				toLeft = true;
+				current = current.left;
+			} else {
+				toLeft = false;
+				current = current.right;
+			}
+
+			if (current === null){
+				return false;
+			}
+		}
+
+		if (current.right === null && current.left === null ){
+			if (current == this.root){
+				this.root = null;
+			} else if (toLeft == true) {
+				parent.left = null;
+			}
+			else {
+				parent.right = null;
+			}
+		} else if (current.right === null){
+			if (current == this.root){
+				this.root = current.left;
+			} else if (toLeft == true){
+				parent.left = current.left;
+			} else {
+				parent.right = current.left;
+			}
+
+		} else if (current.left === null){
+			if (current == this.root){
+				this.root = current.right;
+			} else if (toLeft == true){
+				parent.left = current.right;
+			} else {
+				parent.right = current.right;
+			}
+		} else {
+			var successor = new Node();
+			var l = current.left;
+			var r = current.right;
+			successor = getSuccessor(current);
+			if (current == this.root){
+				this.root = successor;
+			} else if (toLeft == true){
+				parent.left = successor;
+				parent.right = r;
+			} else {
+				parent.right = successor;
+				parent.left = l;
+			}
+		}
 
 	}
 
 	size() {
+		var count = 0;
+		function mySize(localRoot1){
+			if (localRoot1 != null){
+				count++;
+				mySize(localRoot1.left);
+				mySize(localRoot1.right);
+			}
+			return count;
+		};
+		var localRoot = this.root;
+		if (localRoot == null){
+			k = 0;
+			return k;
+		} else{
+			var k = mySize(localRoot);
+			return k;
+		}
 
 	}
+
 
 	isEmpty() {
+		if (this.root == null){
+			return true;
+		} else {
+			return false;
+		}
 
 	}
+
 }
